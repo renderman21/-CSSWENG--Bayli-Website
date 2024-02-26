3337407f49351682ad4684ec04812d523675c643
Allen Neo Merto <129864230+nine-4@users.noreply.github.com>
2024-02-26 18:20:10 +0800

    working navbar

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,9 @@ const controller = require("../controllers/controller.js")
 const app = express()
 
 
+app.get("/", controller.getAboutUs)
 
+app.get("/home", controller.getHome)
 
 
 module.exports = app
